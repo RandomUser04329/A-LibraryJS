abd39fc3cb5d124c065aeb7e8d5a0b7870caa57e
2sided <rsalinas24z@yahoo.com>
2025-10-30 17:39:11 -0500

    MORE PROGRESSION..

---
+++ JSfile.js
@@ -0,0 +1,21 @@
+//Users Library
+const userLibrary = []; 
+
+//Button Selectors
+const makeBTN = document.querySelector(".startBTN");
+const removeBTN = document.querySelector(".removeBTN");
+
+//start Page declare
+let startPage; 
+
+let buttonClicked 
+
+
+
+function Book() {
+
+}
+
+function bookLibrary() { 
+
+}
