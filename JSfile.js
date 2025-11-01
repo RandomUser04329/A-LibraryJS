1b723562591f39a5fce3e1986ea0cfe865ac938a
2sided <rsalinas24z@yahoo.com>
2025-10-31 20:25:25 -0500

    A lil progress

--- JSfile.js
+++ JSfile.js
@@ -12,19 +12,38 @@ function bookLibrary() {
 
 }
 
+//Page Selectors
+const sideContentPage = document.querySelector(".sideCont");
+const libraryContentPage = document.querySelector(".LibraryCont");
+
+
+//Shelf Selectors
+let shelfOne = document.querySelector(".shelf1");
+let shelfTwo = document.querySelector(".shelf2");
+let shelfThree = document.querySelector(".shelf3");
+let shelfFour = document.querySelector(".shelf4");
+let shelfFive = document.querySelector(".shelf5");
+
 //Button Selectors
 const makeBTN = document.querySelector(".startBTN");
 const removeBTN = document.querySelector(".removeBTN");
 
-//start Page declare
-let startPage; 
 //The pages for book creation
+let inputPage; 
+let colorPage; 
 
-makeBTN.addEventListener("click", bookMake());
+makeBTN.addEventListener("click", bookMake(true));
 
-function bookMake() {
-    startPage = document.querySelector(".startSect");
+function bookMake(removePage) {
 
-    
+    //The pages for book creation
+    let inputPage; 
+    let colorPage; 
+  
+    if (removePage == true) {
+        let startPage = document.querySelector(".startSect");
 
+        sideContentPage.classList.remove(startPage);
+
+    }
 }
